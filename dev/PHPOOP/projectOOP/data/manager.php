d72fb85d1cc491174dfc273503d9d9e187b68255
manawalab <3ngkaumar@gmail.com>
2022-02-22 13:21:32 +0800

    Add files via upload
    22februari2022

--- dev/PHPOOP/projectOOP/data/manager.php
+++ dev/PHPOOP/projectOOP/data/manager.php
@@ -1,16 +1,33 @@
 <?php
 
 //pewarisan
-class Manager {
+class manager {
     var string $nama;
+    var string $judul;
+
+    public function __construct (string $nama ="", string $judul = "Manager Baru") {
+        $this->nama = $nama;
+        $this->judul = $judul;
+
+}
+
+
 
     function sayHello (string $nama): void
     {
-        echo "Hy $nama, Nama saya $this->nama" .PHP_EOL;
+        echo "Hy $nama, Nama saya Manajer $this->nama" .PHP_EOL;
     }
 }
 
-class Presiden extends Manager {
+class Presiden extends manager {
+    
+    public function __construct (string $nama = "") {
+    parent::__construct($nama, "Presiden");
+    }
 
+    function sayHello (string $nama): void
+    {
+        echo "Hy $nama, Nama saya Presiden $this->nama" .PHP_EOL;
+    }
 }
 
--- dev/PHPOOP/projectOOP/destructor.php
+++ dev/PHPOOP/projectOOP/destructor.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once "data/person.php";
+
 $engka = new person ("Engka", "Gorontalo") ;
+
 $naim = new person ("Naim", "Minahasa") ;
 
 
